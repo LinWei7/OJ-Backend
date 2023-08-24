d719e1ab932988893ae559320ce56f6d31491045
LinWei7 <lw20010909@126.com>
2023-08-24 17:11:59 +0800

    相关接口开发

--- src/test/java/com/lin/springbootinitbackend/SpringbootInitBackendApplicationTests.java
+++ src/test/java/com/lin/springbootinitbackend/SpringbootInitBackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringbootInitBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
