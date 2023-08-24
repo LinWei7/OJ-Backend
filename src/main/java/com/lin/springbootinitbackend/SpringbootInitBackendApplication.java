a96b949e22190e786b52443ad3123079dc3c7cd0
KingJames <lw20010909@126.com>
2023-08-24 14:55:09 +0800

    Initial commit

---
+++ src/main/java/com/lin/springbootinitbackend/SpringbootInitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lin.springbootinitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootInitBackendApplication.class, args);
+	}
+
+}
